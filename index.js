805fb82b9fa07207e5a605eba97ec4781c4c4534
Edwin <ecro98.98@gmail.com>
2024-06-20 15:56:35 -0500

    Algoritmo inicial

---
+++ index.js
@@ -0,0 +1,270 @@
+const datosPrueba = 
+[
+    {
+      "cod_medico": 84,
+      "name_medico": "ACOSTA GODOY MAGALY CECILIA",
+      "orden_servicio": 1115379,
+      "orden_detalle": 4066448,
+      "id_servicio": 857,
+      "servicio": "CONSULTA PSICOLOGÍA",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "14:20:00",
+      "hora_atencion": "14:35:56",
+      "hc": 279810,
+      "identificacion": "0963025598",
+      "names_paciente": "ESTEVES BAUER JOAQUIN EDUARDO",
+      "sucursal_atencion": "MediLink Urdesa",
+      "empresa": "CONTECON GUAYAQUIL S.A.",
+      "convenio": "PLAN VITAL VITALPLAN S.A.",
+      "valor_seguro": 0.00,
+      "orden_copago": 17.46,
+      "convenio_ajuste": null,
+      "orden_pvp": 17.46,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 86,
+      "name_medico": "AGUILERA ROMERO GLENDA PATRICIA",
+      "orden_servicio": 1113179,
+      "orden_detalle": 4060706,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "17:20:00",
+      "hora_atencion": "17:24:13",
+      "hc": 108678,
+      "identificacion": "0955766209",
+      "names_paciente": "MUÑOZ ALVARADO IGNACIO ALEJANDRO",
+      "sucursal_atencion": "MediLink Sur",
+      "empresa": "PARTICULARES",
+      "convenio": "PARTICULARES",
+      "valor_seguro": 0.00,
+      "orden_copago": 21.00,
+      "convenio_ajuste": null,
+      "orden_pvp": 21.00,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 86,
+      "name_medico": "AGUILERA ROMERO GLENDA PATRICIA",
+      "orden_servicio": 1113072,
+      "orden_detalle": 4060448,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "16:00:00",
+      "hora_atencion": "16:41:52",
+      "hc": 306104,
+      "identificacion": "0930398672",
+      "names_paciente": "MIRANDA BECILLA VICTORIA VALESKA",
+      "sucursal_atencion": "MediLink Sur",
+      "empresa": "NESTLE ECUADOR S.A.",
+      "convenio": "BMI IGUALAS MEDICAS DEL ECUADOR S.A.",
+      "valor_seguro": 11.00,
+      "orden_copago": 10.00,
+      "convenio_ajuste": null,
+      "orden_pvp": 21.00,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 87,
+      "name_medico": "AGUIRRE BURGOS ROSITA MERCEDES",
+      "orden_servicio": 1099923,
+      "orden_detalle": 4026521,
+      "id_servicio": 982,
+      "servicio": "TONOMETRIA",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "15:40:00",
+      "hora_atencion": "15:12:40",
+      "hc": 413033,
+      "identificacion": "0910231984",
+      "names_paciente": "RODRIGUEZ SALAZAR GLENDA FATIMA",
+      "sucursal_atencion": "MediLink Sur",
+      "empresa": "INDIVIDUAL",
+      "convenio": "PLAN VITAL VITALPLAN S.A.",
+      "valor_seguro": 0.00,
+      "orden_copago": 32.95,
+      "convenio_ajuste": null,
+      "orden_pvp": 32.95,
+      "cod_grupo": 7,
+      "grupo_nombre": "PROCEDIMIENTOS"
+    },
+    {
+      "cod_medico": 87,
+      "name_medico": "AGUIRRE BURGOS ROSITA MERCEDES",
+      "orden_servicio": 1099923,
+      "orden_detalle": 4026522,
+      "id_servicio": 1179,
+      "servicio": "GONIOSCOPIA",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "16:00:00",
+      "hora_atencion": "15:06:08",
+      "hc": 413033,
+      "identificacion": "0910231984",
+      "names_paciente": "RODRIGUEZ SALAZAR GLENDA FATIMA",
+      "sucursal_atencion": "MediLink Sur",
+      "empresa": "INDIVIDUAL",
+      "convenio": "PLAN VITAL VITALPLAN S.A.",
+      "valor_seguro": 0.00,
+      "orden_copago": 37.24,
+      "convenio_ajuste": null,
+      "orden_pvp": 37.24,
+      "cod_grupo": 7,
+      "grupo_nombre": "PROCEDIMIENTOS"
+    },
+    {
+      "cod_medico": 87,
+      "name_medico": "AGUIRRE BURGOS ROSITA MERCEDES",
+      "orden_servicio": 1099923,
+      "orden_detalle": 4026520,
+      "id_servicio": 705,
+      "servicio": "FONDO DE OJO",
+      "fecha_atencion": "2024-05-02",
+      "hora_agenda": "15:20:00",
+      "hora_atencion": "15:15:30",
+      "hc": 413033,
+      "identificacion": "0910231984",
+      "names_paciente": "RODRIGUEZ SALAZAR GLENDA FATIMA",
+      "sucursal_atencion": "MediLink Sur",
+      "empresa": "INDIVIDUAL",
+      "convenio": "PLAN VITAL VITALPLAN S.A.",
+      "valor_seguro": 0.00,
+      "orden_copago": 37.55,
+      "convenio_ajuste": null,
+      "orden_pvp": 37.55,
+      "cod_grupo": 7,
+      "grupo_nombre": "PROCEDIMIENTOS"
+    },
+    {
+      "cod_medico": 89,
+      "name_medico": "ALARCON OVIEDO LERBER ASDRUBAL",
+      "orden_servicio": 1114257,
+      "orden_detalle": 4063853,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-01",
+      "hora_agenda": "19:20:00",
+      "hora_atencion": "18:20:39",
+      "hc": 143536,
+      "identificacion": "0931875504",
+      "names_paciente": "BELTRAN FIALLOS BRENDA GABRIELA",
+      "sucursal_atencion": "MediLink Norte",
+      "empresa": "MEDILINK S.A.",
+      "convenio": "LATINA SALUD COMPANIA DE MEDICINA PREPAGADA S.A.",
+      "valor_seguro": 12.00,
+      "orden_copago": 6.00,
+      "convenio_ajuste": null,
+      "orden_pvp": 18.00,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 89,
+      "name_medico": "ALARCON OVIEDO LERBER ASDRUBAL",
+      "orden_servicio": 1112250,
+      "orden_detalle": 4058013,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-01",
+      "hora_agenda": "18:20:00",
+      "hora_atencion": "18:44:05",
+      "hc": 150659,
+      "identificacion": "0932784960",
+      "names_paciente": "ABARCA BARCO GIANLUCA C",
+      "sucursal_atencion": "MediLink Norte",
+      "empresa": "OTROS - SALUD",
+      "convenio": "SALUDSA SISTEMA DE MEDICINA PREPAGADA DEL ECUADOR S.A.",
+      "valor_seguro": 9.50,
+      "orden_copago": 5.50,
+      "convenio_ajuste": 12.80,
+      "orden_pvp": 15.00,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 89,
+      "name_medico": "ALARCON OVIEDO LERBER ASDRUBAL",
+      "orden_servicio": 1111600,
+      "orden_detalle": 4056190,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-01",
+      "hora_agenda": "17:00:00",
+      "hora_atencion": "17:31:28",
+      "hc": 199487,
+      "identificacion": "0923182786",
+      "names_paciente": "MEDINA GUAMAN MAYRA ALEJANDRA",
+      "sucursal_atencion": "MediLink Norte",
+      "empresa": "PARTICULARES",
+      "convenio": "PARTICULARES",
+      "valor_seguro": 0.00,
+      "orden_copago": 21.00,
+      "convenio_ajuste": null,
+      "orden_pvp": 21.00,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    },
+    {
+      "cod_medico": 89,
+      "name_medico": "ALARCON OVIEDO LERBER ASDRUBAL",
+      "orden_servicio": 1112888,
+      "orden_detalle": 4060104,
+      "id_servicio": 9,
+      "servicio": "CONSULTA DERMATOLOGÍA",
+      "fecha_atencion": "2024-05-01",
+      "hora_agenda": "17:40:00",
+      "hora_atencion": "17:58:44",
+      "hc": 309369,
+      "identificacion": "0954556593",
+      "names_paciente": "MAGALLAN MORLA YULIANA LISBETH",
+      "sucursal_atencion": "MediLink Norte",
+      "empresa": "MEXICHEM ECUADOR S.A.",
+      "convenio": "MEXICHEM ECUADOR S.A.",
+      "valor_seguro": 12.80,
+      "orden_copago": 4.00,
+      "convenio_ajuste": null,
+      "orden_pvp": 16.80,
+      "cod_grupo": 2,
+      "grupo_nombre": "CONSULTAS"
+    }
+];
+
+
+
+const tipoServicios =  ["CONSULTA", "PROCEDIMIENTO"];
+
+
+function getAllPagarConsultaMedico(cod_doctor, cod_grupo, valorPago){
+    let nrVeces = 0;        
+    for(let i=0; datosPrueba.length > i; i++){
+        if (datosPrueba[i].cod_grupo === cod_grupo && datosPrueba[i].cod_medico === cod_doctor){
+            nrVeces ++
+        }
+    }
+    var pagarDoctor = nrVeces * valorPago;
+    console.log(`El doctor ha atendido ${nrVeces} servicios y ganará ${pagarDoctor}$`);
+}
+
+
+function getAllPagarProcedimientosMedico(cod_doctor, cod_grupo, porcentajePago){
+    let nrVeces = 0;
+    let valoresPVP = [];        
+    for(let i=0; datosPrueba.length > i; i++){
+        if (datosPrueba[i].cod_grupo === cod_grupo && datosPrueba[i].cod_medico === cod_doctor){
+            nrVeces ++;
+            valoresPVP.push(datosPrueba[i].orden_pvp);
+        }
+    }
+    // Calcular el total de valoresPVP
+    
+    let totalPVP = valoresPVP.reduce((acc, val) => acc + val, 0);
+    // Calcular el pago al doctor basado en el porcentaje
+    let pagarDoctor = totalPVP * (porcentajePago);
+    console.log(`El doctor ha atendido ${nrVeces} procedimientos y ganará ${pagarDoctor.toFixed(2)}$`);
+}
+
+getAllPagarConsultaMedico(89, 2, 9);
+getAllPagarProcedimientosMedico(87, 7, 0.3)
--- main.py
+++ main.py
@@ -1,34 +1,106 @@
 import pandas as pd
-import warnings
+import psycopg2
+from datetime import datetime, timedelta
+import requests
+import base64
 
-# Suprimir advertencias específicas de openpyxl
-warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
+# Conexión a la base de datos
+conn = psycopg2.connect(
+    dbname="samtest",
+    user="postgres",
+    password="Qtza5039",
+    host="192.168.1.95",  
+    port="5432"
+)
 
-# Ruta al archivo Excel
-archivo_excel = './excelHonorarios/honorarios.xlsx'
-hoja = 'PRODUCCION' 
-columnas_por_nombre = ['medico', 'sucursal_nombre_comercial', 'CONCEOTO', 'servicio', 'hora_atencion', 'fecha_atencion']
 
-try:
-    # Leer la hoja especificada del archivo Excel
-    data = pd.read_excel(archivo_excel, sheet_name=hoja)
-    
-    # Leer columnas específicas por nombre
-    data_columnas_por_nombre = data[columnas_por_nombre]
-    
-    # Convertir el DataFrame a JSON y almacenar en una variable
-    resultados_json = data_columnas_por_nombre.to_json(orient='records', lines=True)
-    
-    print("Columnas leídas por nombre:")
-    print(data_columnas_por_nombre.head())
-    
-    # Imprimir los resultados almacenados en la variable JSON (opcional)
-    print("\nResultados JSON:")
-    print(resultados_json)
+fecha_actual = datetime.now()
 
-except FileNotFoundError:
-    print(f"El archivo {archivo_excel} no fue encontrado.")
-except KeyError as e:
-    print(f"Una de las columnas especificadas no existe: {e}")
-except Exception as e:
-    print(f"Ocurrió un error: {e}")
+# Restar un día
+ayer = fecha_actual - timedelta(days=1)
+
+# Formatear como solo fecha, sin tiempo
+fecha_ayer = ayer.date()
+
+fecha_formateada = fecha_ayer.strftime("%d-%m-%Y")
+
+
+# Ejecutar la consulta
+sql = '''select
+    sus.cod_usuario as cod_medico,
+    pmed.cli_nombres_completos name_medico,
+    ode.cod_orden_servicio orden_servicio,
+    ode.cod_orden_detalle orden_detalle,
+    spe.cod_servicio_procedimiento id_servicio,
+    spe.serv_nombre servicio,
+    ode.lab_fecha_atendida :: date fecha_atencion,
+    ac.fecha_inicia:: time hora_agenda,
+    ode.lab_fecha_atendida :: time hora_atencion,
+    pac.cod_sistema_externo hc,
+    pac.cli_no_identificacion identificacion,
+    pac.cli_nombres_completos names_paciente,
+    es.sucursal_nombre_comercial as sucursal_atencion,
+    en3.cli_razon_social empresa,
+    case when en2.cod_tipo_comportamiento = 2 then en1.cli_razon_social else en2.cli_razon_social end convenio,
+    ode.orden_convenio valor_seguro,
+    ode.orden_copago,
+    ode.convenio_ajuste,
+    ode.orden_pvp
+
+from demo.orden_detalles ode
+         inner join demo.servicios_procedimientos spe on spe.cod_servicio_procedimiento = ode.cod_servicio_procedimiento
+         inner join demo.ordenes_servicio ose on ose.cod_orden_servicio = ode.cod_orden_servicio
+         inner join demo.personas_admisiones pam on pam.cod_persona_admision = ose.cod_persona_admision
+         inner join demo.cli_empresas en3 on en3.cod_empresa = pam.cod_empresa_cliente
+         inner join demo.cli_empresas en2 on en2.cod_empresa = en3.cod_empresa_principal
+         left join demo.cli_empresas en1 on en1.cod_empresa = en2.cod_empresa_principal
+         inner join demo.personas pac on pac.cod_persona = pam.cod_persona_paciente
+         inner join demo.seg_usuarios sus on sus.cod_usuario = ode.cod_usuario
+         inner join demo.personas pmed on pmed.cod_persona = sus.cod_persona
+         inner join demo.empresas_sucursales es on es.cod_sucursal = ose.cod_sucursal
+         left join demo.agenda_citas ac ON ac.cod_orden_detalle = ode.cod_orden_detalle
+where date(ode.lab_fecha_atendida) between '2024-05-01' and '2024-05-02' and ord_origen not in ('FARMACIA') limit 10;'''
+df = pd.read_sql(sql, conn)
+
+# Guardar el DataFrame en un archivo Excel en la ruta especificada
+ruta_archivo = r"C:\Users\Desarrollo\Desktop\"+str(fecha_formateada)+".xlsx"
+df.to_excel(ruta_archivo, index=False)
+
+conn.close()
+
+# Leer el archivo en modo binario
+with open(ruta_archivo, 'rb') as archivo:
+    datos_binarios = archivo.read()
+
+# Codificar los datos binarios en base64
+datos_base64 = base64.b64encode(datos_binarios)
+
+# Convertir a una cadena de caracteres para poder manipular o enviar como texto
+cadena_base64 = datos_base64.decode('utf-8')
+# Enviar reporte por Correo 
+url = 'http://192.168.2.14:8089/api/send'
+response = requests.get(url)
+datos = {
+  "destinatario": [
+    "eromero@medilink.com.ec", "dcarrera@medilink.com.ec"
+  ],
+  "destinatarioCC": [],
+  "asunto": "Producción Médicos",
+  "body": "Reporte de producción médicos.",
+  "adjunto": cadena_base64,
+  "nombre_adjunto": "produccion_"+str(fecha_formateada)+".xlsx"
+}
+
+
+# Realizar la solicitud POST
+response = requests.post(url, json=datos)
+
+# Verificar la respuesta
+if response.status_code == 200 or response.status_code == 201:
+    print("Email enviado exitosamente")
+    # Aquí puedes imprimir la respuesta JSON si la API devuelve algo relevante
+    print(response.json())
+else:
+    print("Error al enviar el email:", response.status_code)
+    # Es útil imprimir el cuerpo de la respuesta para entender el error
+    print(response.text)
